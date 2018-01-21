dd5dcd57328f92bb7fcfd8724f80cb230087eb01
kristru <krissafuentes@gmail.com>
2018-01-21 12:34:42 -0700

    firebase reference issue

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RoomsList from './components/RoomsList.js'
 import * as firebase from 'firebase';
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCWRm3pBRh7mvmvVFTIDPv5PNV15b13xaQ",
+    authDomain: "chat-e-012018.firebaseapp.com",
+    databaseURL: "https://chat-e-012018.firebaseio.com",
+    projectId: "chat-e-012018",
+    storageBucket: "chat-e-012018.appspot.com",
+    messagingSenderId: "133229647795"
+  };
+
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App container-fluid">
+        <div className="App-Content container row">
+
+          <section className="Left-Dashboard col-lg-4">
+            <header className="App-header row">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+            <RoomsList />
+          </section>
+
+          <section className="Message-Board col-lg-8"></section>
+
+        </div>
       </div>
     );
   }
--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -1,11 +1,27 @@
 import React, {Component} from 'react';
 
-class RoomsList extends Component{
 
-}
+ class RoomsList extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      rooms:[1:shopping, 2:sports, 3:cooking],
+    };
+  }
 
-render(){
+  this.roomsRef = this.props.firebase.database().ref('rooms');
 
+  componentDidMount() {
+     this.roomsRef.on('child_added', snapshot => {
+       console.log(snapshot);
+     });
+   }
+
+  render(){
+    return(
+      <div>Rooms List Will Goes here</div>
+    );
+  }
 }
 
 export default RoomsList;
