cfcabdd537ca4c833d5ac4dd933df52f9dd92944
kristru <krissafuentes@gmail.com>
2018-01-23 20:19:11 -0700

    connecting form to handlers

--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -7,26 +7,50 @@ import React, {Component} from 'react';
     this.roomsRef = this.props.firebase.database().ref('rooms');
     this.state = {
       rooms:[],
+      newRoomName: ''
     };
   }
 
   componentDidMount(room) {
+    //firebase read function (.on) of Database snapshots
      this.roomsRef.on('child_added', snapshot => {
-       console.log(snapshot);
+       //sets room to a snapshot
        const room = snapshot.val();
+       //updates the dom by creating a new array with the room
        this.setState({rooms: this.state.rooms.concat( room )});
      });
    }
 
+   handleChange(e){
+     this.setState({newRoomName:e.target.value});
+     console.log(e.target.value);
+   }
+
+   handleSubmit(e) {
+     e.preventDefault();
+     //push() input value to this.roomsRef
+     console.log(e.target.value);
+   }
+
   render(){
     return(
-      <ul className="rooms-list">
-        {
-          this.state.rooms.map((room, index) =>
-            <li className='room' key={index}>{room.name}</li>
-          )
-        }
-      </ul>
+      <section>
+        <form onSubmit={ (e) => this.handleSubmit(e) }>
+          <input type="text"
+            name="name"
+            value={this.state.newRoomName}
+            onChange={ (e) => this.handleChange(e)}
+          />
+          <input type="submit" value="Create New Room" />
+        </form>
+        <ul className="rooms-list">
+          {
+            this.state.rooms.map((room, index) =>
+              <li className='room' key={ index }>{room.name}</li>
+            )
+          }
+        </ul>
+      </section>
     );
   }
 }
