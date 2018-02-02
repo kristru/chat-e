852497b657d7f7968eb00300b2953ba4ea585699
kristru <krissafuentes@gmail.com>
2018-02-01 20:56:03 -0700

    filter function psuedo code

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -15,6 +15,11 @@ class MessageList extends Component {
    };
  }
 
+ //invoked before a mounted component recieves a new props
+ componentWillReceiveProps(nextProps) {
+   //console.log(nextProps.activeRoom);
+   }
+
 //this mounts the information to the dom
  componentDidMount(message) {
    //adding a trigger or event listener for every child added
@@ -26,17 +31,29 @@ class MessageList extends Component {
     });
   }
 
+updateDisplayedMessages(activeRoom) {
+  console.log(this.props.activeRoom);
+
+  //const results will change to setState
+  const results = this.state.messages.filter((message)=> message.roomId === this.props.activeRoom);
+  //this is what displayedMessages will be
+  console.log(results);
+
+}
+
  render(){
   return(
     <section>
+      <h1>You&apos;re in room: {this.props.activeRoom}</h1>
       <section>
-        <ul className="message-list">
+        <ul className="message-list" id="activeRoom">
           {
             this.state.messages.map((message, index) =>
-              <li className="message" key={index}>
+              <li className="message" key={index} onClick={this.updateDisplayedMessages()}>
                 <div className="content">{message.content}</div>
                 <div className="username">from: {message.username}</div>
                 <div className="timestamp">sent:{message.sentAt}</div>
+                <div className="timestamp">roomId:{message.roomId}</div>
               </li>
             )
           }
